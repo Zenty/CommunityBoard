ec4193e6dd10a8e36e5f98a82ef59026453e26cf
Pontus Flodström <pontaflod95@hotmail.com>
2025-09-20 12:34:19 +0200

    Added Pages + Routing for 'create-post', 'index', and 'login'

---
+++ src/pages/IndexPage.tsx
@@ -0,0 +1,15 @@
+NotFoundPage.route = {
+  path: '/'
+};
+
+export default function NotFoundPage() {
+  return <>
+    <h2>Welcome To The Community Board</h2>
+    <p>
+      Where anyone can use this platform to post community announcements,
+      events, services, and more. Our goal is to foster a sense of community
+      and provide a space for individuals and organizations to connect and
+      share information.
+    </p>
+  </>;
+}
