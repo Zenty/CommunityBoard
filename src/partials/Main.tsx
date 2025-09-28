7b9907f82a8bac78b41807899ca1f8c111b68062
Pontus Flodström <pontaflod95@hotmail.com>
2025-09-28 14:18:07 +0200

    Create an interface UserData.

--- src/partials/Main.tsx
+++ src/partials/Main.tsx
@@ -1,17 +1,19 @@
+import type UserData from '../interfaces/UserData.ts';
 import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 type HeaderProps = {
     isUser: boolean;
     isAdmin: boolean;
+    userData: UserData | null;
     isDarkMode: boolean;
     setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
   };
 
-export default function Main({isUser, isAdmin}: HeaderProps) {
+export default function Main({isUser, isAdmin, userData}: HeaderProps) {
   return <main>
     <Container className="p-4">
-      <Outlet context={{ isUser, isAdmin }} />
+      <Outlet context={{ isUser, isAdmin, userData }} />
     </Container>
   </main>;
 }
